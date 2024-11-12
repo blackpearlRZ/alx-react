6e5131e244c790c6c2a6325b6e4d24db2967fd68
Chaimae aafif <138730457+blackpearlRZ@users.noreply.github.com>
2024-11-12 21:04:05 +0100

    inline styling

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const headerStyle = {
+    backgroundColor: '#deb5b545'
+};
+
+const rowStyle = {
+    backgroundColor: '#f5f5f5ab'
+};
+
 const CourseListRow = ({ isHeader = false, textFirstCell, textSecondCell = null }) => {
     return (
-        <tr>
+        <tr style={rowStyle}>
             {isHeader ?
                 (textSecondCell === null ?
-                    <th colSpan={2}>{textFirstCell}</th> :
-                    <><th>{textFirstCell}</th><th>{textSecondCell}</th></>)
+                    <th style={headerStyle} colSpan={2}>{textFirstCell}</th> :
+                    <>
+                        <th style={headerStyle}>{textFirstCell}</th>
+                        <th style={headerStyle}>{textSecondCell}</th>
+                    </>
+                )
                 : <><td>{textFirstCell}</td><td>{textSecondCell}</td></>}
         </tr>
     )
