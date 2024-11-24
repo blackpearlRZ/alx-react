0348dc96b22e474025d121edfe70e9cb2542c800
Chaimae aafif <138730457+blackpearlRZ@users.noreply.github.com>
2024-11-24 22:24:42 +0100

    react components

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.css'
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
@@ -7,6 +7,8 @@ import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
 
 
 class App extends React.Component {
@@ -52,7 +54,9 @@ class App extends React.Component {
           <Header />
         </div>
         <div className="App-body">
-          {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          {this.props.isLoggedIn ? <BodySectionWithMarginBottom title = 'Course list'><CourseList listCourses={listCourses} /></BodySectionWithMarginBottom>: 
+          <BodySectionWithMarginBottom title="Log in to continue"><Login /></BodySectionWithMarginBottom>}
+          <BodySection title="News from the School"><p>Chaimae Aafif is the BEST developer ever! She'll conquer the world!</p></BodySection>
         </div>
         <div className="App-footer">
           <Footer />
