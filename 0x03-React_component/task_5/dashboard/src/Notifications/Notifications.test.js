6dd38719b060942aef1a675dea4def403df5e0f7
Chaimae aafif <138730457+blackpearlRZ@users.noreply.github.com>
2024-11-26 20:48:59 +0100

    react component

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Notifications from './Notifications';
 import NotificationItem from './NotificationItem';
 
@@ -16,12 +15,12 @@ describe('<Notifications />', () => {
 
     it('renders correct text', () => {
         const wrapper = shallow(<Notifications displayDrawer={true} />);
-        expect(wrapper.find(NotificationItem).html()).toBe('<li data=\"default\">No new notification for now</li>');
+        expect(wrapper.find(NotificationItem).dive().text()).toContain('No new notification for now');
     })
 
     it('renders first NotificationItem properly', () => {
         const wrapper = shallow(<Notifications displayDrawer={true} />);
-        expect(wrapper.find(NotificationItem).first().html()).toBe('<li data=\"default\">No new notification for now</li>');
+        expect(wrapper.find(NotificationItem).at(0).dive().text()).toBe('No new notification for now');
     })
 
     it('menu item is being displayed when displayDrawe is false', () => {
@@ -57,13 +56,59 @@ describe('<Notifications />', () => {
         const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
         expect(wrapper.find(NotificationItem).exists()).toBe(true);
         expect(wrapper.find(NotificationItem).length).toBe(1);
-        expect(wrapper.find(NotificationItem).html()).toBe('<li data=\"default\">AbduHanyCourse</li>');
+        expect(wrapper.find(NotificationItem).dive().text()).toContain('AbduHanyCourse');
     });
 
     it('correct messgae is displayed', () => {
         const wrapper = shallow(<Notifications listNotifications={[]} displayDrawer={true} />);
         expect(wrapper.find(NotificationItem).exists()).toBe(true);
-        expect(wrapper.find(NotificationItem).html()).toBe('<li data=\"default\">No new notification for now</li>');
+        expect(wrapper.find(NotificationItem).dive().text()).toContain('No new notification for now');
         expect(wrapper.find('div.Notifications').text()).not.toContain('Here is the list of notifications');
+    });
+
+    it('mocks console.log function', () => {
+        const spy = jest.spyOn(console, 'log');
+        const wrapper = mount(<Notifications listNotifications={[
+            { id: 1, type: "default", value: "Notification 01" },
+        ]} displayDrawer={true} />);
+        const instance = wrapper.instance();
+        instance.markAsRead(1);
+        expect(spy).toHaveBeenCalledWith('Notification 1 has been marked as read');
+        spy.mockRestore();
+    });
+
+    it('doesnt rerender when same list is given', () => {
+        const initialNotifications = [
+            { id: 1, type: "default", value: "Notification 01" },
+            { id: 2, type: "default", value: "Notification 02" },
+        ];
+
+        const shortNotifications = [
+            { id: 1, type: "default", value: "Notification 01" },
+        ];
+        const wrapper = shallow(<Notifications listNotifications={initialNotifications} displayDrawer={true} />);
+        const instance = wrapper.instance();
+        jest.spyOn(instance, 'render');
+        wrapper.setProps({ listNotifications: shortNotifications });
+        expect(instance.render).toHaveBeenCalledTimes(0);
+    });
+
+    it('doesnt rerender when same list is given', () => {
+        const initialNotifications = [
+            { id: 1, type: "default", value: "Notification 01" },
+            { id: 2, type: "default", value: "Notification 02" },
+        ];
+
+        const longerNotification = [
+            { id: 1, type: "default", value: "Notification 01" },
+            { id: 2, type: "default", value: "Notification 02" },
+            { id: 3, type: "default", value: "Notification 03" },
+            { id: 4, type: "default", value: "Notification 04" },
+        ]
+        const wrapper = shallow(<Notifications listNotifications={initialNotifications} displayDrawer={true} />);
+        const instance = wrapper.instance();
+        jest.spyOn(instance, 'render');
+        wrapper.setProps({ listNotifications: longerNotification });
+        expect(instance.render).toHaveBeenCalledTimes(1);
     });
 });
